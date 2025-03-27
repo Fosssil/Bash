64a0b5a02e9b7222247f8f5726d479ed1aa52cdb
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-03-27 18:05:27 +0530

    Thursday 27 March 2025 06:05:27 PM IST

--- update_2.sh
+++ update_2.sh
@@ -2,7 +2,7 @@
 echo -e "\033[31m\033[1mPurging lock files...\033[0m"
 sudo rm -rf /var/lib/dpkg/lock-frontend /var/lib/dpkg/lock /var/cache/apt/archives/lock /var/cache/apt/archives/
 
-echo -e "\033[31m\033[1mUpdating Repos...\033[0m"
+echo -e "\033[0;32m\033[1mUpdating Repos...\033[0m"
 echo ""
 sudo apt autoremove -y &&
   sudo apt autoclean &&
@@ -10,14 +10,13 @@ sudo apt autoremove -y &&
   sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 echo ""
-echo -e "\033[0;32mChecking for required packages...\033[0m"
+echo -e "\033[0;32m\033[1mChecking for required packages...\033[0m"
 package=("git" "dialog" "ansible-core" "neovim")
-echo ""
-
 for pkg in "${package[@]}"; do
   echo -e "+ $pkg"
   package_to_install+=("$pkg")
 done
+echo ""
 
 function install {
   for package_install in "${package_to_install[@]}"; do
