2ef73c6e8111aec8ae1537794dc58621ee1e2c2e
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-03-28 14:59:38 +0530

    Friday 28 March 2025 02:59:38 PM IST

--- update_2.sh
+++ update_2.sh
@@ -31,7 +31,8 @@ wait
 
 printf "\n"
 echo "Cloning the Nvim Configs"
-git clone https://github.com/Fosssil/nvim.git "$HOME/.config/nvim"
+git clone https://github.com/Fosssil/nvim.git "$HOME/.config/nvim" || true
+
 
 # HEIGHT=15
 # WIDTH=40
