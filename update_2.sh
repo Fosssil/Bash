9fcc69bcbf85463214eabf790456d362ad6e8e9e
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-03-28 16:27:28 +0530

    Friday 28 March 2025 04:27:28 PM IST

--- update_2.sh
+++ update_2.sh
@@ -6,10 +6,9 @@ reset="\033[0m"
 read -rp "$(echo -e "${green}Enter your Github Username: ${reset}")" git_username
 # read -rsp "$(echo -e "${green}Enter your Github Token (input will be hidden): ${reset}")" git_token
 # echo "$git_token"
-printf "\n"
-
-# Read token from ~/token file
 token_file="$HOME/token"
+echo -e "${green}Getting token file from $token_file...${reset}"
+
 if [[ -f "$token_file" ]]; then
   git_token=$(tr -d '[:space:]' <"$token_file") # Read and trim whitespace
   if [[ -z "$git_token" ]]; then
