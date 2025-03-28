d71606949c76129fc30aa4a00b140e7f6bc15536
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-03-28 15:17:40 +0530

    Friday 28 March 2025 03:17:40 PM IST

--- update_2.sh
+++ update_2.sh
@@ -7,16 +7,16 @@ echo -e "${red}Purging lock files...${reset}"
 sudo rm -rf /var/lib/dpkg/lock-frontend /var/lib/dpkg/lock /var/cache/apt/archives/lock /var/cache/apt/archives/
 
 echo ""
-echo -e "${green}Updating Repos...${reset}"
+echo -e "${green} Updating Repos...${reset}"
 sudo apt autoremove -y &&
   sudo apt autoclean &&
   sudo apt update &&
   echo ""
-echo -e "${green}Adding unstable neovim PPA...${reset}"
+echo -e "${green} Adding unstable neovim PPA...${reset}"
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 echo ""
-echo -e "${green}Checking for required packages...${reset}"
+echo -e "${green} Checking for required packages...${reset}"
 package=("git" "dialog" "ansible-core" "neovim")
 for pkg in "${package[@]}"; do
   echo -e "+ $pkg"
@@ -34,7 +34,7 @@ sudo apt install "${package_to_install[@]}" -y
 wait
 
 printf "\n"
-echo "${green}Cloning the Nvim Configs${reset}"
+echo -e "${green} Cloning the Nvim Configs${reset}"
 git clone https://github.com/Fosssil/nvim.git "$HOME/.config/nvim" || true
 
 # HEIGHT=15
