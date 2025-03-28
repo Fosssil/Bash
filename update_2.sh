e80e15991cb35b888fea8e609f4d982932a11513
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-03-28 16:24:57 +0530

    Friday 28 March 2025 04:24:57 PM IST

--- update_2.sh
+++ update_2.sh
@@ -4,10 +4,22 @@ green="\033[0;32m\033[1m"
 reset="\033[0m"
 
 read -rp "$(echo -e "${green}Enter your Github Username: ${reset}")" git_username
-echo "$git_username"
-read -rsp "$(echo -e "${green}Enter your Github Token (input will be hidden): ${reset}")" git_token
-echo "$git_token"
+# read -rsp "$(echo -e "${green}Enter your Github Token (input will be hidden): ${reset}")" git_token
+# echo "$git_token"
 printf "\n"
+
+# Read token from ~/token file
+token_file="$HOME/token"
+if [[ -f "$token_file" ]]; then
+  git_token=$(tr -d '[:space:]' <"$token_file") # Read and trim whitespace
+  if [[ -z "$git_token" ]]; then
+    echo -e "${red}Error: $token_file is empty. Please add your GitHub token to it.${reset}"
+    exit 1
+  fi
+else
+  echo -e "${red}Error: $token_file not found. Please create it with your GitHub token.${reset}"
+  exit 1
+fi
 
 printf "\n"
 echo -e "${red}Purging lock files...${reset}"
