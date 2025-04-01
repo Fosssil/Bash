7e83b476a296556fe682e22fde66e5c35f054e52
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-04-01 18:29:27 +0530

    Tuesday 01 April 2025 06:29:27 PM IST

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Color variables
 red="\033[31m\033[1m"
 green="\033[0;32m\033[1m"
-# yellow="\033[33m"
-yellow="\033[0;36m\033[1m"
+yellow="\033[33m\033[1m"
+# yellow="\033[0;36m\033[1m"
 reset="\033[0m"
 
 # Hardcoded defaults (configurable via env vars)
