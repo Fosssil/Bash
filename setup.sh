9a402d7d289ed1f6bb00c2862699a9f8301e1ce0
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-04-02 14:37:09 +0530

    Wednesday 02 April 2025 02:37:09 PM IST

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ else
 fi
 
 # Getting number of upgradable packages
-sudo apt list --upgradable | grep --regexp=[0-9] -c "upgradable"
+sudo apt list --upgradable | grep -c "upgradable" | grep --regexp=[0-9]*
 
 # Add unstable Neovim PPA
 print_section "Adding unstable Neovim PPA..."
